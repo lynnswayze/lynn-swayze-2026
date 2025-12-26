8898b51c06c824eca8240e356c1697e7f6f2ed23
Lynn Swayze <lynnswayze@gmail.com>
2025-12-26 17:20:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ if ( url.includes("posts/") ) {
 let headerHTML = '<ul> <li><a href="' + relativePath + '/index.html">Home</a></li>' + 
 '<li><a href="' + relativePath + '/now.html">Now</a></li>' +
 '<li><a href="' + relativePath + '/about.html">About</a></li>' +
-'<li><a href="' + relativePath + '/fiction.html">Lynn's Books</a></li>' + 
+'<li><a href="' + relativePath + '/fiction.html">Books</a></li>' + 
 '<li><a href="' + relativePath + '/services.html">Marketing Services</a></li>' + 
 '<li><a href="' + relativePath + '/archive.html">Archive</a></li>' + 
 '<li><a href="' + relativePath + '/contact.html">Contact</a></li></ul>';
