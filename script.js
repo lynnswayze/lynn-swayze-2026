f0bfde7a6a689054f9f190452ba9f4b26b08d02c
Lynn Swayze <lynnswayze@gmail.com>
2025-12-26 14:17:29 -0500

    fixed index page
    updates etc

--- script.js
+++ script.js
@@ -154,7 +154,7 @@ postListHTML += "</ul>";
 
 //Generate the Recent Post List HTML, which can be shown on the home page (or wherever you want!)
 let recentPostsCutoff = 9; //Hey YOU! Change this number to set how many recent posts to show before cutting it off with a "more posts" link.
-let recentPostListHTML = "<h2>Recent Posts:</h2><ul>";
+let recentPostListHTML = "<h1>Latest Posts:</h1><ul>";
 let numberOfRecentPosts = Math.min( recentPostsCutoff, postsArray.length );
 for ( let i = 0; i < numberOfRecentPosts; i++ ) {
   recentPostListHTML += formatPostLink(i);
